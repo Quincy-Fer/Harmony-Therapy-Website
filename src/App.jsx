9574c7e7dcee4944c492c563f6846d8c4f6c4887
Quincy Fer <quincy.e.fer@gmail.com>
2024-06-27 20:50:50 +0200

    Create title website, create jsx files for components and sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
-function App() {
-  return <><h1 className="text-green-500 font-bold">Hello World</h1></>;
+const App = () => {
+  return (
+    <>
+      <h1 className="text-green-500 font-bold">Hello World</h1>
+    </>
+  );
 }
 
 export default App;
