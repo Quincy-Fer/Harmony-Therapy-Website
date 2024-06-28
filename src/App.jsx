2196dbad80c6f9d555614b51e595678b829f1bd4
Quincy Fer <quincy.e.fer@gmail.com>
2024-06-28 15:17:45 +0200

    Add css reset, import google font

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,9 @@ import NavBar from "./Components/NavBar";
 const App = () => {
   return (
     <>
+    <h1 className="">Beep Boop</h1>
+    <h1 className="">Beep Boop</h1>
+    <h1 className="">Beep Boop</h1>
       <NavBar />
       <Hero />
       <About />
