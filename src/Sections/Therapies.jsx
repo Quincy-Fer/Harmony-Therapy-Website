10b1b396a8265c28cfce7b2cc4e052cf3d5c2121
Quincy Fer <quincy.e.fer@gmail.com>
2024-07-01 22:26:41 +0200

    Center h and p

--- src/Sections/Therapies.jsx
+++ src/Sections/Therapies.jsx
@@ -5,17 +5,17 @@ import { chevronLeft, chevronRight, quotes } from "../assets/icons";
 const Therapies = () => {
   return (
     <section className="max-w-[1440px] mx-auto pt-20" id="therapies">
-      <div className="flex justify-start gap-48">
-        <div className="flex flex-col ">
+      <div className="flex justify-center gap-48">
+        <div className="flex flex-col items-center">
           <h2
             className="text-stone-700 text-[60px] max-w-[720px] font-bold font-quicksand
-         leading-tight tracking-tight"
+         leading-tight tracking-tight text-center"
           >
             Tailored Therapies for Your Needs
           </h2>
           <p
             className="font-nunito max-w-[500px] text-base text-gray-500 tracking-wider leading-loose 
-          mt-10  "
+          mt-10 text-center "
           >
             Our diverse therapy options are designed to support your mental
             health journey:
