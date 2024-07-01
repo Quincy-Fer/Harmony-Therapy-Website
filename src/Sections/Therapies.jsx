91e6a5ecff7a454a5ba4e3e2f658374e59b17471
Quincy Fer <quincy.e.fer@gmail.com>
2024-07-01 21:35:00 +0200

    Replace icons

--- src/Sections/Therapies.jsx
+++ src/Sections/Therapies.jsx
@@ -1,11 +1,12 @@
 import { THERAPIES } from "../constants";
 import TherapyCard from "../Components/TherapyCard";
+import { chevronLeft, chevronRight } from "../assets/icons";
 
 const Therapies = () => {
   return (
     <section className="max-w-[1440px] mx-auto pt-20" id="therapies">
-      <div className="flex justify-center">
-        <div className="flex flex-col text-center">
+      <div className="flex justify-start gap-48">
+        <div className="flex flex-col ">
           <h2
             className="text-stone-700 text-[60px] max-w-[720px] font-bold font-quicksand
          leading-tight tracking-tight"
@@ -14,16 +15,21 @@ const Therapies = () => {
           </h2>
           <p
             className="font-nunito max-w-[500px] text-base text-gray-500 tracking-wider leading-loose 
-          mt-10 self-center "
+          mt-10  "
           >
             Our diverse therapy options are designed to support your mental
             health journey:
           </p>
         </div>
+
+        <div className="flex h-20 w-20 gap-4 rounded-3xl place-items-end">
+          <img src={chevronLeft} alt="button to scroll left" className="w-12" />
+          <img src={chevronRight} alt="button to scroll right" className="w-12" />
+        </div>
       </div>
       <div className="flex overflow-x-auto gap-10 hide-scrollbar">
         {THERAPIES.map((therapy) => (
-          <TherapyCard 
+          <TherapyCard
             key={therapy.key}
             title={therapy.title}
             description={therapy.description}
