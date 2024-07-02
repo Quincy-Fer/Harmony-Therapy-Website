7ff2eac9fd4a594367ecd1bdb0b32995df5ebc07
Quincy Fer <quincy.e.fer@gmail.com>
2024-07-02 16:33:04 +0200

    Start testimonial section

--- src/Sections/Testimonials.jsx
+++ src/Sections/Testimonials.jsx
@@ -1,5 +1,10 @@
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <section className="w-full mx-auto border border-green-600 bg-white mt-20">
+      <h2>Hear from Our Clients</h2>
+      <h5>Positive Changes, Real Results</h5>
+    </section>
+  );
 };
 
 export default Testimonials;
