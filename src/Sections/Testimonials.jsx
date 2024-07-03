9bafe3bca928437f9845b6ad4a9eca7cad5bc819
Quincy Fer <quincy.e.fer@gmail.com>
2024-07-03 15:03:04 +0200

    Add blur on bottom of div, change section bg and h/p color

--- src/Sections/Testimonials.jsx
+++ src/Sections/Testimonials.jsx
@@ -3,41 +3,44 @@ import TestimonialCard from "../Components/TestimonialCard";
 
 const Testimonials = () => {
   return (
-    <section
-      className="w-full mx-auto h-[1168px] bg-green-100  mt-20 rounded-3xl overflow-hidden"
-      id="testimonial"
-    >
-      {/* heading and paragraph */}
-      <div className="flex flex-col justify-start mx-10">
-        <h2
-          className="text-stone-700 text-[60px]  font-bold font-quicksand
+    <>
+      <section
+        className="w-full mx-auto h-[1168px] bg-green-700  mt-20 rounded-3xl overflow-hidden relative "
+        id="testimonial"
+      >
+        {/* heading and paragraph */}
+        <div className="flex flex-col justify-start mx-10">
+          <h2
+            className="text-lime-100 text-[60px]  font-bold font-quicksand
          leading-tight tracking-tight mt-10"
-        >
-          What our clients say
-        </h2>
-        <p
-          className="font-nunito max-w-[600px] text-base text-gray-600 tracking-wider leading-loose 
+          >
+            What our clients say
+          </h2>
+          <p
+            className="font-nunito max-w-[600px] text-base text-lime-100 italic tracking-wider leading-loose 
           mt-2"
-        >
-          Positive Changes, Real Results
-        </p>
-      </div>
-
-      {/* cards */}
-      <div className="mt-2 p-5">
-        <div className="columns-sm ml-10  ">
-          {TESTIMONIALS.map((testimonial) => (
-            <TestimonialCard
-              key={testimonial.key}
-              therapy={testimonial.therapy}
-              name={testimonial.name}
-              quote={testimonial.quote}
-              imgURL={testimonial.img}
-            />
-          ))}
+          >
+            Positive Changes, Real Results
+          </p>
         </div>
-      </div>
-    </section>
+        {/* cards */}
+        <div className="mt-2 p-5">
+          <div className="columns-sm ml-10  ">
+            {TESTIMONIALS.map((testimonial) => (
+              <TestimonialCard
+                key={testimonial.key}
+                therapy={testimonial.therapy}
+                name={testimonial.name}
+                quote={testimonial.quote}
+                imgURL={testimonial.img}
+              />
+            ))}
+          </div>
+        </div>
+        {/* Gradient on the bottom */}
+        <div className="absolute w-full h-20 -mt-[145px] bg-gradient-to-r from-emerald-700 via bg-green-300 to-green-700 blur-[30px] "></div>
+      </section>
+    </>
   );
 };
 
