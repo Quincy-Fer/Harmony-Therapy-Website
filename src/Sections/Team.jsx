0010b033f95301ac54e152d4837de9a8a6e1f287
Quincy Fer <quincy.e.fer@gmail.com>
2024-07-04 21:09:28 +0200

    Adjust image sizings for team. Great job :)

--- src/Sections/Team.jsx
+++ src/Sections/Team.jsx
@@ -1,4 +1,4 @@
-import { TEAM } from "../constants";
+import { TEAM} from "../constants";
 import TeamCard from "../Components/TeamCard";
 import { useState } from "react";
 
@@ -17,23 +17,21 @@ const Team = () => {
     <section className="max-w-[1440px] mx-auto pt-20" id="team">
       {/* Heading Subheading */}
       <div className="flex flex-col">
-        <div className="">
-          <h2
-            className="text-stone-700 text-[60px]  font-bold font-quicksand
+        <h2
+          className="text-stone-700 text-[60px]  font-bold font-quicksand
          leading-tight tracking-tight "
-          >
-            The Harmony Team
-          </h2>
-          <p
-            className="font-nunito max-w-[600px] text-base text-gray-500 tracking-wider leading-loose 
+        >
+          The Harmony Team
+        </h2>
+        <p
+          className="font-nunito max-w-[600px] text-base text-gray-500 tracking-wider leading-loose 
           mt-5 "
-          >
-            Providing Support and Expertise for Every Step of Your Journey
-          </p>
-        </div>
+        >
+          Providing Support and Expertise for Every Step of Your Journey
+        </p>
 
         {/* Image and Text */}
-        <div className="flex flex-row mt-10">
+        <div className="flex mt-10">
           <div className="flex-1 ">
             <p
               className="font-nunito max-w-[640px]  text-gray-900 tracking-wide 
@@ -49,11 +47,11 @@ const Team = () => {
               Click an image below to learn more about our therapists
             </p>
           </div>
-          <div className="flex-1">
+          <div className="flex-1 ">
             <img
               src={selectedContent.imgURL}
               alt={selectedContent.alt}
-              className="rounded-3xl object-cover"
+              className="rounded-3xl max-h-[400px] min-w-[400px] "
             />
           </div>
         </div>
