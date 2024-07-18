e90734daf3537890f760d67d37982ca3fc0a6e59
Quincy Fer <quincy.e.fer@gmail.com>
2024-07-18 13:30:19 +0200

    Adjust final cta

--- src/Sections/FinalCTA.jsx
+++ src/Sections/FinalCTA.jsx
@@ -12,22 +12,22 @@ const FinalCTA = () => {
 
   return (
     <section className="w-full mt-20 bg-green-400 mx-auto" id="contact">
-      <div className="flex">
+      <div className="grid md:grid-cols-2">
         {/* img left */}
-        <div className="flex-1 p-10 rounded-3xl my-10">
-          <img src={cta} alt="Contact foto" className="rounded-3xl" />
+        <div className=" p-10 rounded-3xl my-10">
+          <img src={cta} alt="Contact foto" className="rounded-3xl h-full object-cover" />
         </div>
         {/* Right col */}
-        <div className="flex-1 p-20">
+        <div className=" p-20">
           <div className=" ">
             <h2
-              className="text-white text-[60px] max-w-[600px] font-bold font-quicksand
+              className="text-white md:text-[44px] lg:text-[72px] lg:max-w-[780px] font-bold font-quicksand
          leading-tight tracking-tight "
             >
               Ready to Take the Next Step?
             </h2>
             <p
-              className="font-nunito max-w-[600px] text-lg  text-white tracking-wider leading-loose
+              className="font-nunito max-w-[600px] text-base  text-white tracking-wider leading-loose
           mt-10 mb-10 "
             >
               We're here to answer your questions and help you start your
