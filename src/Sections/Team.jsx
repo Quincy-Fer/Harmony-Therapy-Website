1c048962dd51d4ea9f314d5ad8cbbb0dd9ef3755
Quincy Fer <quincy.e.fer@gmail.com>
2024-07-19 20:01:30 +0200

    Switch image position team, mobile, hide portraits

--- src/Sections/Team.jsx
+++ src/Sections/Team.jsx
@@ -29,14 +29,20 @@ const Team = () => {
         <p
           className="hidden lg:block font-nunito max-w-[600px] text-base text-gray-500 tracking-wider leading-loose 
           mt-5 "
-
         >
           Providing Support and Expertise for Every Step of Your Journey
         </p>
 
         {/* Image and Text */}
-        <div className="grid lg:grid-cols-2 mt-4 lg:mt-10 ">
+        <div className="grid lg:grid-cols-2  lg:mt-10 ">
           <div className="">
+            <div className="md:hidden flex ml-4 mt-5 ">
+              <img
+                src={selectedContent.imgURL}
+                alt={selectedContent.alt}
+                className="rounded-3xl object-cover object-center max-w-[300px]  "
+              />
+            </div>
             <p
               className="font-nunito md:max-w-[640px]  text-gray-900 tracking-wide 
           text-lg mr-4 max-sm:p-4 "
@@ -44,7 +50,7 @@ const Team = () => {
               {selectedContent.text}
             </p>
           </div>
-          <div className=" md:mt-4 lg:mt-0 ">
+          <div className="hidden md:block md:mt-4 lg:mt-0 ">
             <img
               src={selectedContent.imgURL}
               alt={selectedContent.alt}
@@ -54,13 +60,13 @@ const Team = () => {
         </div>
         <p
           className="font-nunito max-w-[640px]  text-gray-600 tracking-wide 
-          text-balance text-lg italic mt-5"
+          text-balance text-lg italic mt-5 hidden md:block"
         >
           Click an image below to learn more about our therapists
         </p>
       </div>
       {/* IMAGES map */}
-      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-4 mt-4">
+      <div className="md:grid md:grid-cols-2 lg:grid-cols-4 gap-4 mt-4 hidden">
         {TEAM.slice(1).map((item) => (
           <TeamCard
             key={item.key}
